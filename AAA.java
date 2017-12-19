3e1238428ba88a3a1108c7f339e0be3c74d2c9fd
Pirapu Raj <ppirapu@gmail.com>
2017-12-18 21:50:19 -0800

    one new method is added

--- AAA.java
+++ AAA.java
@@ -11,4 +11,15 @@ int avg = 0;
 avg = Sum/7;
 		System.out.print("Average=" + Sum/7.0--  );
 	}
+
+public void strCompare(){
+String s1="Sachin";  
+   String s2="Sachin";  
+   String s3=new String("Sachin");  
+   String s4="Saurav";  
+   System.out.println(s1.equals(s2));//true  
+   System.out.println(s1.equals(s3));//true  
+   System.out.println(s1.equals(s4));//false  
+}   
+
 }
