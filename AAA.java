85ffd56f4b00ca14e798eb1e0f2e5063aa12b239
Pirapu Raj <ppirapu@gmail.com>
2017-12-18 20:13:59 -0800

    file is modified

---
+++ AAA.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class AAA{
+	public static void main(String args[]){
+		int My[]={20,30,25,35,-16,60,-100};
+		int Sum =0;
+int avg = 0;
+		for(int i=0; i<7; i++){
+			Sum= Sum+My[i];
+			
+		}
+avg = Sum/7;
+		System.out.print("Average=" + Sum/7.0--  );
+	}
+}
